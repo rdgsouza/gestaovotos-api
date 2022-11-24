0f53d41fe7370db6e88c0658a2317986b820d2d3
Rodrigo Souza <rdsouza.c@gmail.com>
2022-11-24 11:49:39 -0400

    Iciando o Projeto e adicionando as dependências necessárias

---
+++ gestaovotos-api/src/main/java/br/com/gestaovotos/GestaovotosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gestaovotos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaovotosApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestaovotosApiApplication.class, args);
+    }
+
+}
